2568a6456e202f22f71eefacb7c08d6c4bf6e970
huynhtl <huynhtl@vng.com.vn>
2025-02-16 17:49:42 +0700

    add test limit buy and test limit sell

--- src/trade_page.py
+++ src/trade_page.py
@@ -122,6 +122,136 @@ class TradePage:
         notification_title = self.wait.until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='notification-title']")))
         return notification_title.text
+
+    def limit_buy_price(self, unit_value, stop_loss_price_value, take_profit_price_value,
+                        trace_input_price_value):
+        time.sleep(2)
+        # Click on buy button
+        buy = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-button-order-buy']")))
+        buy.click()
+
+        # Select order type dropdown
+        order_type = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-dropdown-order-type']")))
+        order_type.click()
+
+        # Select order type limit
+        limit_order_type = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-dropdown-order-type-limit']")))
+        limit_order_type.click()
+
+        # Enter size
+        size = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-input-volume']")))
+        size.click()
+        size.send_keys(unit_value)
+
+        # Enter price
+        trace_input_price = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-input-price']")))
+        trace_input_price.click()
+        trace_input_price.send_keys(trace_input_price_value)
+
+        # Enter stop loss
+        stop_loss = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-input-stoploss-price']")))
+        stop_loss.click()
+        stop_loss.send_keys(stop_loss_price_value)
+
+        # Enter take profit
+        take_profit = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-input-takeprofit-price']")))
+        take_profit.click()
+        take_profit.send_keys(take_profit_price_value)
+
+        # Select expiry
+        expiry_dropdown = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-dropdown-expiry']")))
+        expiry_dropdown.click()
+
+        good_till_cancelled = self.wait.until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "[data-testid='trade-dropdown-expiry-good-till-cancelled']")))
+        good_till_cancelled.click()
+
+        buy_order = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-button-order']")))
+        buy_order.click()
+
+        # Confirm order
+        confirm_button = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-confirmation-button-confirm']")))
+        confirm_button.click()
+
+        notification_title = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='notification-title']")))
+        print(notification_title.text)
+        return notification_title.text
+
+    def limit_sell_price(self, unit_value, stop_loss_price_value, take_profit_price_value,
+                         trace_input_price_value):
+        time.sleep(2)
+        # Click on buy button
+        buy = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-button-order-sell']")))
+        buy.click()
+
+        # Select order type dropdown
+        order_type = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-dropdown-order-type']")))
+        order_type.click()
+
+        # Select order type limit
+        limit_order_type = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-dropdown-order-type-limit']")))
+        limit_order_type.click()
+
+        # Enter size
+        size = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-input-volume']")))
+        size.click()
+        size.send_keys(unit_value)
+
+        # Enter price
+        trace_input_price = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-input-price']")))
+        trace_input_price.click()
+        trace_input_price.send_keys(trace_input_price_value)
+
+        # Enter stop loss
+        stop_loss = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-input-stoploss-price']")))
+        stop_loss.click()
+        stop_loss.send_keys(stop_loss_price_value)
+
+        # Enter take profit
+        take_profit = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-input-takeprofit-price']")))
+        take_profit.click()
+        take_profit.send_keys(take_profit_price_value)
+
+        # Select expiry
+        expiry_dropdown = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-dropdown-expiry']")))
+        expiry_dropdown.click()
+
+        good_till_cancelled = self.wait.until(
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "[data-testid='trade-dropdown-expiry-good-till-cancelled']")))
+        good_till_cancelled.click()
+
+        buy_order = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-button-order']")))
+        buy_order.click()
+
+        # Confirm order
+        confirm_button = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='trade-confirmation-button-confirm']")))
+        confirm_button.click()
+
+        notification_title = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='notification-title']")))
+        print(notification_title.text)
+        return notification_title.text
     
     def validate_order_history(self, unit_value, stop_loss_price_value, take_profit_price_value, order_type_value):
         order_history = self.wait.until(
@@ -147,6 +277,136 @@ class TradePage:
                 return True
 
         return False
+    
+    def validate_pending_order(self, unit_value, stop_loss_price_value, take_profit_price_value, order_type_value, expiry_value):
+        # Click on pending order tab
+        pending_order_tab = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='tab-asset-order-type-pending-orders']")))
+        pending_order_tab.click()
+
+        time.sleep(3)
+        rows = self.driver.find_elements(
+            By.CSS_SELECTOR,
+            "table[data-testId='asset-pending-table'] tbody[data-testId='asset-pending-list'] tr"
+        )
+
+        for row in rows:
+            profit = row.find_element(By.CSS_SELECTOR, "td[data-testId='asset-pending-column-take-profit']").text
+            stop_loss = row.find_element(By.CSS_SELECTOR, "td[data-testId='asset-pending-column-stop-loss']").text
+            unit = row.find_element(By.CSS_SELECTOR, "td[data-testId='asset-pending-column-units']").text
+            order_type = row.find_element(By.CSS_SELECTOR,
+                                          "td[data-testId='asset-pending-column-order-type'] span").text
+            expiry = row.find_element(By.CSS_SELECTOR, "td[data-testId='asset-pending-column-expiry']").text
+
+            if unit == unit_value and order_type == order_type_value and expiry == expiry_value and int(
+                    float(profit.replace(',', ''))) == int(take_profit_price_value) and int(
+                    float(stop_loss.replace(',', ''))) == int(stop_loss_price_value):
+                return True
+
+        return False
+
+    def get_pending_order(self):
+        # Click on pending order tab
+        pending_order_tab = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='tab-asset-order-type-pending-orders']")))
+        pending_order_tab.click()
+
+        time.sleep(3)
+        rows = self.driver.find_elements(
+            By.CSS_SELECTOR,
+            "table[data-testId='asset-pending-table'] tbody[data-testId='asset-pending-list'] tr"
+        )
+
+        for row in rows:
+            profit = row.find_element(By.CSS_SELECTOR, "td[data-testId='asset-pending-column-take-profit']").text
+            stop_loss = row.find_element(By.CSS_SELECTOR, "td[data-testId='asset-pending-column-stop-loss']").text
+            unit = row.find_element(By.CSS_SELECTOR, "td[data-testId='asset-pending-column-units']").text
+            order_type = row.find_element(By.CSS_SELECTOR,
+                                          "td[data-testId='asset-pending-column-order-type'] span").text
+            expiry = row.find_element(By.CSS_SELECTOR, "td[data-testId='asset-pending-column-expiry']").text
+
+            return profit, stop_loss, unit, order_type
+
+        return None
+
+    def edit_pending_order(self, price_value, stop_loss_price_value, take_profit_price_value, is_change_expiry: bool):
+        edit = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='asset-pending-button-edit']")))
+        edit.click()
+
+        price = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='edit-input-price']")))
+        price.click()
+        price.send_keys(Keys.CONTROL + "a")
+        price.send_keys(Keys.BACKSPACE)
+        price.send_keys(price_value)
+
+        stop_loss_price = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='edit-input-stoploss-price']")))
+        stop_loss_price.click()
+        stop_loss_price.send_keys(Keys.CONTROL + "a")
+        stop_loss_price.send_keys(Keys.BACKSPACE)
+        stop_loss_price.send_keys(stop_loss_price_value)
+
+        take_profit = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='edit-input-takeprofit-price']")))
+        take_profit.click()
+        take_profit.send_keys(Keys.CONTROL + "a")
+        take_profit.send_keys(Keys.BACKSPACE)
+        take_profit.send_keys(take_profit_price_value)
+
+        if is_change_expiry:
+            expiry_value = self.driver.find_element(
+                By.CSS_SELECTOR, "[data-testid='edit-dropdown-expiry'] div div"
+            )
+            print(expiry_value.text)
+            if expiry_value.text == "Good Till Cancelled":
+                expiry = self.wait.until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='edit-dropdown-expiry']")))
+                expiry.click()
+
+                expiry_gtc = self.wait.until(
+                    EC.presence_of_element_located(
+                        (By.CSS_SELECTOR, "[data-testid='edit-dropdown-expiry-good-till-day']")))
+                expiry_gtc.click()
+
+            elif expiry_value.text == "Good Till Day":
+                expiry = self.wait.until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='edit-dropdown-expiry']")))
+
+                expiry.click()
+
+                expiry_gtd = self.wait.until(
+                    EC.presence_of_element_located(
+                        (By.CSS_SELECTOR, "[data-testid='edit-dropdown-expiry-good-till-cancelled']")))
+                expiry_gtd.click()
+
+        update_button = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='edit-button-order']")))
+        update_button.click()
+
+        confirm_button = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='edit-confirmation-button-confirm']")))
+        confirm_button.click()
+        notification_title = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='notification-title']")))
+        print(notification_title.text)
+        return notification_title.text
+    
+    def close_pending_order(self):
+        # Click on delete button
+        delete_button = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='asset-pending-button-close']")))
+        delete_button.click()
+
+        # Confirm delete
+        delete_button_confirm = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Delete Order")]')))
+        delete_button_confirm.click()
+
+        notification_title = self.wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='notification-title']")))
+        return notification_title.text
 
     def get_order_history(self):
         order_history = self.wait.until(
---
+++ tests/test_limit_buy.py
@@ -0,0 +1,125 @@
+import time
+from enum import Enum
+
+import pytest
+from selenium import webdriver
+from src.login_page import LoginPage
+from src.trade_page import TradePage
+from config.settings import USERNAME, PASSWORD
+
+
+class SearchValues(Enum):
+    SEARCH = "BTCUSD.std"
+    ORDER_TYPE = "BUY LIMIT"
+    EXPIRY_GTC = "Good Till Cancelled"
+    EXPIRY_GTD = "Good Till Day"
+    URL = "https://aqxtrader.aquariux.com/web/login"
+
+
+@pytest.fixture(scope="module")
+def data():
+    return SearchValues
+
+
+@pytest.fixture(scope="module")
+def driver(data):
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(data.URL.value)
+    yield driver
+    # input("Press Enter to close the browser...")
+    driver.quit()
+
+
+@pytest.fixture(scope="module")
+def shared_data():
+    return {}
+
+
+@pytest.fixture(scope="module")
+def login_page(driver):
+    login_page = LoginPage(driver)
+    return login_page
+
+
+@pytest.fixture(scope="module")
+def trade_page(driver):
+    trade_page = TradePage(driver)
+    return trade_page
+
+
+@pytest.mark.dependency()
+def test_login(driver, login_page):
+    login_page.login(USERNAME, PASSWORD)
+
+    # Verify successful login
+    assert "web" in driver.current_url, "Login failed!"
+
+
+@pytest.mark.dependency(depends=["test_login"])
+def test_search(trade_page, data):
+    item = trade_page.search_and_select(data.SEARCH.value)
+
+    assert item is not None, "No search results found!"
+
+
+@pytest.mark.dependency(depends=["test_search"])
+def test_limit_buy_price(trade_page, data, shared_data):
+    time.sleep(3)
+    # Get the current price
+    current_price = trade_page.get_current_price()
+    print(current_price)
+    shared_data["current_price"] = current_price
+    assert current_price is not None, "No current price found!"
+
+    # Set the unit, stop loss, and take profit prices
+    shared_data["unit"] = "0.01"
+    shared_data["stop_loss_price"] = current_price * 0.5
+    shared_data["take_profit_price"] = current_price * 2
+    shared_data["trace_input_price_value"] = current_price * 0.7
+
+    # Buy at market price
+    time.sleep(2.5)
+    noti_title = trade_page.limit_buy_price(shared_data["unit"], shared_data["stop_loss_price"],
+                                            shared_data["take_profit_price"],
+                                            shared_data["trace_input_price_value"])
+    assert noti_title == "Limit Order Submitted", "Market order failed or popup not showed!"
+
+    # Validate the order
+    is_validate = trade_page.validate_pending_order(shared_data["unit"], shared_data["stop_loss_price"],
+                                                    shared_data["take_profit_price"], data.ORDER_TYPE.value,
+                                                    data.EXPIRY_GTC.value)
+    assert is_validate == True, "Order validation failed!"
+
+
+@pytest.mark.dependency(depends=["test_limit_buy_price"])
+def test_limit_edit_buy_price(trade_page, data, shared_data):
+    shared_data["unit_edit"] = shared_data["unit"]
+    shared_data["stop_loss_price_edit"] = shared_data["stop_loss_price"] - shared_data["stop_loss_price"] * 0.1
+    shared_data["take_profit_price_edit"] = shared_data["take_profit_price"] * 1.1
+    shared_data["trace_input_price_value_edit"] = shared_data["trace_input_price_value"] + shared_data[
+        "trace_input_price_value"] * 0.1
+
+    # Edit the order
+    time.sleep(2.5)
+    noti_title = trade_page.edit_pending_order(shared_data["trace_input_price_value"],
+                                               shared_data["stop_loss_price_edit"],
+                                               shared_data["take_profit_price_edit"], True)
+    assert noti_title == "Limit Order Updated", "Market order failed or popup not showed!"
+
+    # Validate the order
+    is_validate = trade_page.validate_pending_order(shared_data["unit_edit"], shared_data["stop_loss_price_edit"],
+                                                    shared_data["take_profit_price_edit"], data.ORDER_TYPE.value,
+                                                    data.EXPIRY_GTD.value)
+    assert is_validate == True, "Order validation failed!"
+
+@pytest.mark.dependency(depends=["test_limit_edit_buy_price"])
+def test_close_order(trade_page, data):
+    time.sleep(2.5)
+    notification_title = trade_page.close_pending_order()
+    assert notification_title == "Delete Order", "Delete order failed or popup not showed!"
+
+@pytest.mark.dependency(depends=["test_close_order"])
+def test_validate_order_history(trade_page, data, shared_data):
+    is_validate = trade_page.validate_order_history(shared_data["unit_edit"], shared_data["stop_loss_price_edit"], shared_data["take_profit_price_edit"], data.ORDER_TYPE.value)
+    assert is_validate == True, "Close order validation failed!"
